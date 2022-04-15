6f373db06534e7168d8ef46535570c9780d34dae
Mojib Mohammad <mmohammd3@outlook.com>
2022-04-15 07:20:53 -0700

    Extract game winner logic to a helper method

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Game {
     // Properties;
-    private String username;
+    private String name;
     private String choice;
     private int score;
     private boolean winner;
@@ -18,19 +18,20 @@ public class Game {
 
         // Computer stuff
         Game computer = new Game();
-        computer.setUsername("Computer");
+        computer.setName("Computer");
 
         // Player stuff
         System.out.println("Please enter your name?");
         Game player = new Game();
         String playerName = scanner.nextLine();
-        player.setUsername(playerName);
+        player.setName(playerName);
 
         while(winner == false) {
             String computerChoice = getRandomChoice();
             computer.setChoice(computerChoice);
 
-            System.out.println("Please select a choice?");
+            System.out.println("Please select a choice:");
+            System.out.println();
             logChoices();
 
             String choice = scanner.nextLine();
@@ -38,29 +39,40 @@ public class Game {
             // r => R + ock
             player.setChoice(choice.substring(0, 1).toUpperCase() + choice.substring(1));
 
-            if(computer.choice.equals(player.choice)) {
-                System.out.println("It's a Tie!");
-            }
-            if(computer.choice.equals("Rock") && player.choice.equals("Scissors") ||
-                    computer.choice.equals("Scissors") && player.choice.equals("Paper") ||
-                    computer.choice.equals("Paper") && player.choice.equals("Rock")) {
-
-                computer.score++;
-                System.out.println(computer.username + "'s score: " + computer.score);
-            } else {
-                player.score++;
-                System.out.println(player.username + "'s score: " + player.score);
-            }
-            if(computer.score >= 3) {
-                System.out.println(computer.username + " is the winner");
-                winner = true;
-            }
-            if(player.score >= 3) {
-                System.out.println(player.username + " is the winner");
-                winner = true;
-            }
+            determineWinner(player, computer);
         }
 
+    }
+
+    private void determineWinner(Game player, Game computer) {
+        System.out.println();
+        System.out.println("You choose " + player.choice);
+        System.out.println("Computer choose " + computer.choice);
+        System.out.println();
+
+         if(computer.choice.equals(player.choice)) {
+            System.out.println("It's a Tie!");
+        }
+
+         else if(computer.choice.equals("Rock") && player.choice.equals("Scissors") ||
+                computer.choice.equals("Scissors") && player.choice.equals("Paper") ||
+                computer.choice.equals("Paper") && player.choice.equals("Rock")) {
+
+            computer.score++;
+            System.out.println(computer.name + "'s score: " + computer.score);
+        } else {
+            player.score++;
+            System.out.println(player.name + "'s score: " + player.score);
+        }
+        if(computer.score >= 3) {
+            System.out.println(computer.name + " is the winner");
+            winner = true;
+        }
+
+        if(player.score >= 3) {
+            System.out.println(player.name + " is the winner");
+            winner = true;
+        }
     }
 
     private String getRandomChoice() {
@@ -83,10 +95,10 @@ public class Game {
         return (input.equals("Rock") || input.equals("Paper") || input.equals("Scissors"));
     }
 
-    public void setUsername(String username) {
-        if(username.length() <= 3)
+    public void setName(String name) {
+        if(name.length() <= 3)
             throw new IllegalArgumentException();
-        else this.username = username;
+        else this.name = name;
     }
 
     public void setChoice(String player1Choice) {
